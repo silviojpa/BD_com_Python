013bcfa7abee7601bca1a1102603340920bb27ea
silviojpa <silviop.ezequiel@gmail.com>
2025-08-14 03:19:32 -0300

    Commit inicial: Com o projeto BD com Python

---
+++ 01. Postgree/4.Transact-SQL/6. Enviando Dados para o Postgree (Colunas Distintas).sql
@@ -0,0 +1,26 @@
+   CREATE TABLE IF NOT EXISTS anac_mapeamento (
+        ID int,
+        Classificacao_da_Ocorrencia VARCHAR(50),
+        Dt_Ocorrencia DATE,
+        Municipio VARCHAR(50),
+        UF VARCHAR(30),
+        Regiao VARCHAR(30),
+        Fabricante VARCHAR(100)
+        ---Modele VARCHAR(50)
+    )
+	
+
+
+
+    	SELECT column_name
+FROM information_schema.columns
+WHERE table_name = 'anac_mapeamento';
+
+
+ALTER TABLE anac_mapeamento
+ADD COLUMN nome_da_nova_coluna tipo_de_dados;
+
+--ALTER TABLE anac_mapeamento
+--ADD Modelo varchar(50)
+
+
